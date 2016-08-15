a4211a327781c9d80b6211ff02c4bd9baf3126e8
Prateek Kher <prateek.kher@gmail.com>
2016-08-15 14:09:08 +0530

    Made Some Changes in Customer.html
    Now added ability to add, edit and remove Customers

--- core/js/controllers/customers.controller.js
+++ core/js/controllers/customers.controller.js
@@ -1,12 +1,17 @@
 app.controller('CustomerController', ['$scope', '$http', function($scope, $http){
-    $scope.customers = [];
     $scope.activePage = 1;
     $scope.itemsPerPage = 5;
     $scope.queryText = "";
+    $scope.selectedCustomer = {
+        "name" : "",
+        "address" : ""
+    };
+
+    $scope.isSelected = false;
 
     
     $scope.getCustomers = function(){
-        $scope.customers = [];
+        $scope.$parent.customers = [];
         $scope.pages = [];
 
         var req = {
@@ -19,13 +24,74 @@ app.controller('CustomerController', ['$scope', '$http', function($scope, $http)
         .success( function( res ){
             // console.log( res );
             angular.forEach(res.serverData, function(item){
-                $scope.customers.push( item );
+                $scope.$parent.customers.push( item );
             });
         })
 
         .then(function(){
-            $scope.$broadcast('Paginate', $scope.customers.length);
+            $scope.$broadcast('Paginate', $scope.$parent.customers.length);
         });
+    }
+
+    $scope.addNew = function(){
+        var request = {
+            "queryType" : "insert",
+            "tableName" : "customers",
+            "params" : {
+                "columnNames" : ['name', 'address'],
+                "userData" : { "name" : $scope.selectedCustomer.name, "address" : $scope.selectedCustomer.address }
+            }
+        }
+
+        $scope.save( request );
+    }
+
+    $scope.updateCustomer = function(){
+        var request = {
+            "queryType" : "update",
+            "tableName" : "customers",
+            "params" : {
+                "columnNames" : ['name', 'address'],
+                "userData" : { "name" : $scope.selectedCustomer.name, "address" : $scope.selectedCustomer.address },
+                "conditions" : {"id" : $scope.selectedCustomer.id }
+            }
+        }
+
+        $scope.save( request );
+    }
+
+    $scope.save = function(request){
+        $http.post( LINK, request )
+
+        .success( function(response){
+            if( response['affectedRows'] == 1 ){
+                $scope.selectedCustomer.id = response.lastInsertId;
+                $scope.$parent.customers.push( $scope.selectedCustomer );
+                $scope.selectedCustomer = {
+                    "name" : "",
+                    "address" : ""
+                }
+
+                $scope.isSelected = false;
+            }
+        });
+    }
+
+    $scope.selectCustomer = function(customer){
+        var index = $scope.$parent.customers.indexOf( customer );
+        $scope.selectedCustomer = customer;
+        $scope.$parent.customers.splice(index, 1);
+        $scope.isSelected = true;
+    }
+
+    $scope.unSelect = function(){
+        $scope.$parent.customers.push( $scope.selectedCustomer );
+        $scope.selectedCustomer = {
+            "name" : "",
+            "address" : ""
+        }
+
+        $scope.isSelected = false;
     }
 
     $scope.$on('PageChange', function(event, data){
--- core/js/directives.js
+++ core/js/directives.js
@@ -2,12 +2,19 @@ app.directive('sideBar', function(){
     return {
         'restrict' : 'E',
         'templateUrl' : 'components/sidebar.html'
-    }
+    };
 });
 
 app.directive('tableHeader', function(){
     return {
         'restrict' : 'E',
         'templateUrl' : 'components/tableHeader.html'
-    }
+    };
+});
+
+app.directive('customerTable', function(){
+    return {
+        'restrict' : 'E',
+        'templateUrl' : 'components/customerTable.html'
+    };
 })
